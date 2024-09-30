99821703419885fa77a0b935aa561787ce8ab69a
GdeGimenez <gdegimenez07@gmail.com>
2024-09-30 09:07:57 +0200

    Funcion dividir

--- calculadora.js
+++ calculadora.js
@@ -3,3 +3,16 @@ let num2 = parseInt(prompt("Dime el número 2"));
 
 let suma = num1 + num2;
 console.log("Suma: " + suma);
+
+let resta = num1 - num2;
+console.log("Resta: " +resta);
+
+let multiplicacion = num1 * num2;
+console.log("Multiplicacion: " +multiplicacion);
+
+if (num2 == 0){
+    console.log("Error");
+}else{
+    let division = num1 / num2;
+    console.log("Division: " +division);
+}
